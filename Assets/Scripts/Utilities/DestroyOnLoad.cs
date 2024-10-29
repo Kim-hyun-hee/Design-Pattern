f32ec81f76cf977511fd391e8fb1f292f781bd12
Kim-hyun-hee <asdfzxcv0730@naver.com>
2024-10-29 23:22:07 +0900

    [Utilities] 씬 로드 시 오브젝트 상태 변경하는 스크립트 추가

---
+++ Assets/Scripts/Utilities/DestroyOnLoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyOnLoad : MonoBehaviour
+{
+    [SerializeField]
+    private List<string> activeWithinScenes;
+
+    [Tooltip("지정되지 않은 경우 현재 GameObject로 기본 설정")]
+    [SerializeField]
+    private GameObject objectToDestroy;
+    [SerializeField]
+    private bool debug;
+
+    private void Start()
+    {
+        if (!activeWithinScenes.Contains(SceneManager.GetActiveScene().name))
+        {
+            if (objectToDestroy == null)
+                objectToDestroy = gameObject;
+
+            Destroy(objectToDestroy);
+
+            if (debug)
+            {
+                Debug.Log("Active scene: " + SceneManager.GetActiveScene().name);
+                Debug.Log("Do not destroy in scene: " + string.Join(", ", activeWithinScenes));
+                Debug.Log("Destroy on load: " + objectToDestroy);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/DisableOnLoad.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DisableOnLoad : MonoBehaviour
+{
+    [SerializeField]
+    private List<string> activeWithinScenes;
+
+    [Tooltip("지정되지 않은 경우 현재 GameObject로 기본 설정")]
+    [SerializeField]
+    private GameObject objectToDisable;
+    [SerializeField]
+    private bool debug;
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += SceneManager_SceneLoaded;
+        SceneManager.sceneUnloaded += SceneManager_SceneUnloaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= SceneManager_SceneLoaded;
+        SceneManager.sceneUnloaded += SceneManager_SceneUnloaded;
+    }
+
+    private void Awake()
+    {
+        if (objectToDisable == null)
+            objectToDisable = gameObject;
+    }
+
+    private void SceneManager_SceneLoaded(Scene loadedScene, LoadSceneMode mode)
+    {
+        if (!activeWithinScenes.Contains(loadedScene.name))
+        {
+            DisableGameObject(loadedScene);
+        }
+    }
+
+    private void SceneManager_SceneUnloaded(Scene unloadedScene)
+    {
+        if (activeWithinScenes.Contains(SceneManager.GetActiveScene().name))
+        {
+            EnableGameObject(unloadedScene);
+        }
+    }
+
+    private void EnableGameObject(Scene scene)
+    {
+        objectToDisable.SetActive(true);
+
+        if (debug)
+        {
+            Debug.Log("[DisableOnLoad] re-enabled GameObject: " + objectToDisable.name + "; unloaded scene: " + scene.name);
+        }
+    }
+
+    private void DisableGameObject(Scene scene)
+    {
+        objectToDisable.SetActive(false);
+
+        if (debug)
+        {
+            Debug.Log("[DisableOnLoad]  Disabled GameObject: " + objectToDisable.name + "; loaded scene: " + scene.name);
+        }
+    }
+}
